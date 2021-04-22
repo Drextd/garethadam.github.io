601882db546ba6171ce475854690910aaa63d358
Gareth Adam <gareth.adam@tabcorp.com.au>
2021-04-22 21:42:54 +1000

    Added base Home content

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const cardMarkup = `
     <div class="cardLayoutContainer">
 
         <section class="leftSection">
-            <div id="leftPictureContainer"></div>
+            <div id="leftPictureContainer">
+                <div id="leftPicture"></div>
+            </div>
             <div id="leftPictureTitle"></div>
             <div id="leftExtraInfo"></div> 
         </section>
@@ -19,5 +21,8 @@ const cardMarkup = `
 
 window.addEventListener('DOMContentLoaded', function (){
     document.getElementById('cardDisplayContainer').innerHTML = cardMarkup;
+    document.getElementById('leftPictureTitle').innerHTML = 'Gareth Adam'
+    document.getElementById('leftExtraInfo').innerHTML = 'Web Developer, I.T Professional'
+
 })
 
