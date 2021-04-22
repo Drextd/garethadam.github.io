4e47ba29213580d3fe48d797f9ebec324f2909ef
Gareth Adam <gareth.adam@tabcorp.com.au>
2021-04-22 22:20:57 +1000

    Addition Home content, Event Listeners created for top bar

--- script.js
+++ script.js
@@ -23,6 +23,29 @@ window.addEventListener('DOMContentLoaded', function (){
     document.getElementById('cardDisplayContainer').innerHTML = cardMarkup;
     document.getElementById('leftPictureTitle').innerHTML = 'Gareth Adam'
     document.getElementById('leftExtraInfo').innerHTML = 'Web Developer, I.T Professional'
-
+    document.getElementById('rightAreaTop').innerHTML = 'Welcome to my Portfolio.'
+    document.getElementById('rightAreaMiddle').innerHTML = 'Most recent position held was a Graduate Support Engineer to get my foot in the door after graduating from University of Canberra (QLD TAFE Campus).'
+    document.getElementById('rightAreaBottom').innerHTML = 'Now seeking to take the next step in my career as a Junior Web Developer / Software Engineer.'
 })
+
+document.getElementById('home').addEventListener('click', popluateRightCard_Home)
+document.getElementById('about').addEventListener('click', popluateRightCard_About)
+document.getElementById('github').addEventListener('click', popluateRightCard_Github)
+document.getElementById('resume').addEventListener('click', popluateRightCard_Resume)
+
+function popluateRightCard_Home(){
+    // WIP
+}
+
+function popluateRightCard_About(){
+    // WIP
+}
+
+function popluateRightCard_Github(){
+    // WIP
+}
+
+function popluateRightCard_Resume(){
+    // WIP
+}
 
