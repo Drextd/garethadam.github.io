4cca783f6b73a2d4616eb32d38d4391146a93902
Gareth Adam <gareth.adam@tabcorp.com.au>
2021-04-22 20:02:35 +1000

    Base card layout added

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
 const cardMarkup = `
-    
+    <div class="cardLayoutContainer">
+
+        <section class="leftSection">
+            <div id="leftPictureContainer"></div>
+            <div id="leftPictureTitle"></div>
+            <div id="leftExtraInfo"></div> 
+        </section>
+
+        <section class="rightSection">
+            <div class="rightElementStyling" id="rightAreaTop"></div>
+            <div class="rightElementStyling" id="rightAreaMiddle"></div>
+            <div class="rightElementStyling" id="rightAreaBottom"></div>
+        </section>
+    </div>
 `;
 
 
+window.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('cardDisplayContainer').innerHTML = cardMarkup;
+})
 
-function loadHomeCard(){
-
-    
-
-}
