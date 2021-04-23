b31159334899f24a832a6b27e7ab443a1b60050e
Gareth Adam <gareth.adam@tabcorp.com.au>
2021-04-23 10:13:02 +1000

    Added base About content

--- script.js
+++ script.js
@@ -21,11 +21,7 @@ const cardMarkup = `
 
 window.addEventListener('DOMContentLoaded', function (){
     document.getElementById('cardDisplayContainer').innerHTML = cardMarkup;
-    document.getElementById('leftPictureTitle').innerHTML = 'Gareth Adam'
-    document.getElementById('leftExtraInfo').innerHTML = 'Web Developer, I.T Professional'
-    document.getElementById('rightAreaTop').innerHTML = 'Welcome to my Portfolio.'
-    document.getElementById('rightAreaMiddle').innerHTML = 'Most recent position held was a Graduate Support Engineer to get my foot in the door after graduating from University of Canberra (QLD TAFE Campus).'
-    document.getElementById('rightAreaBottom').innerHTML = 'Now seeking to take the next step in my career as a Junior Web Developer / Software Engineer.'
+    popluateRightCard_Home()
 })
 
 document.getElementById('home').addEventListener('click', popluateRightCard_Home)
@@ -34,11 +30,20 @@ document.getElementById('github').addEventListener('click', popluateRightCard_Gi
 document.getElementById('resume').addEventListener('click', popluateRightCard_Resume)
 
 function popluateRightCard_Home(){
-    // WIP
+    document.getElementById('leftPictureTitle').innerHTML = 'Gareth Adam'
+    document.getElementById('leftExtraInfo').innerHTML = 'Web Developer, I.T Professional'
+    document.getElementById('rightAreaTop').innerHTML = 'Welcome to my Portfolio.'
+    document.getElementById('rightAreaMiddle').innerHTML = 'Most recent position held was a Graduate Support Engineer of 2 years to get my foot in the door into the I.T industry.'
+    document.getElementById('rightAreaBottom').innerHTML = 'Now seeking to take the next step in my career as a Junior Web Developer / Software Engineer.'
 }
 
 function popluateRightCard_About(){
-    // WIP
+    document.getElementById('rightAreaTop').innerHTML = 'A little bit about myself.'
+    document.getElementById('rightAreaMiddle').innerHTML = `After graduating from University of Canberra (QLD TAFE Campus) back in 2018 I went straight into the I.T industry as a Graduate Support Engineer. 
+                                                            This position opened up the world to what it means to work in a larger team and gave me first hand experience working within a Monolithic system.
+                                                            Additionally has granted me a unique perspective of being able on both sides of the fence with supporting applications and developing them.`
+    document.getElementById('rightAreaBottom').innerHTML = `My interests lie within problem solving, running on the treadmill of always learning, curiousity to dive deeper that what is required.
+                                                            Lastly, I spend my free time with my loving family and being an avid gamer (video and tabletop).`
 }
 
 function popluateRightCard_Github(){
